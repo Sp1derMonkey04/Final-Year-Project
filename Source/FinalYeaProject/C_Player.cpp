e979bc7de239ffe66e029ba6afd556802eac95e6
Sp1derMonkey04 <115369325+Sp1derMonkey04@users.noreply.github.com>
2025-02-04 17:09:38 +0000

    blargh

---
+++ Source/FinalYeaProject/C_Player.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_Player.h"
+
+// Sets default values
+AC_Player::AC_Player()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_Player::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AC_Player::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AC_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent
+
+}
+
